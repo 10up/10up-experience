a373a2be373b9cbb33e194c274be9a63a8df53c4
Helen Hou-Sandi <helen.y.hou@gmail.com>
2018-02-06 17:04:41 -0500

    Use `plugins_url()` now that this isn't an `mu-plugin`

--- 10up-experience.php
+++ 10up-experience.php
@@ -69,16 +69,16 @@ add_action( 'admin_bar_menu', 'tenup\add_about_menu', 11 );
 function admin_enqueue_scripts() {
 	global $pagenow;
 
-	wp_enqueue_style( '10up-admin', content_url( 'mu-plugins/10up-experience/assets/css/admin.css' ) );
+	wp_enqueue_style( '10up-admin', plugins_url( '/assets/css/admin.css', __FILE__ ) );
 
 	if ( 'admin.php' === $pagenow && ! empty( $_GET['page'] ) && ( '10up-about' === $_GET['page'] || '10up-team' === $_GET['page'] || '10up-support' === $_GET['page'] ) ) {
-		wp_enqueue_style( '10up-about', content_url( 'mu-plugins/10up-experience/assets/css/tenup-pages.css' ) );
+		wp_enqueue_style( '10up-about', plugins_url( '/assets/css/tenup-pages.css', __FILE__ ) );
 	}
 }
 add_action( 'admin_enqueue_scripts', 'tenup\admin_enqueue_scripts' );
 
 function enqueue_scripts() {
-	wp_enqueue_style( '10up-admin', content_url( 'mu-plugins/10up-experience/assets/css/admin.css' ) );
+	wp_enqueue_style( '10up-admin', plugins_url( '/assets/css/admin.css', __FILE__ ) );
 }
 add_action( 'wp_enqueue_scripts', 'tenup\enqueue_scripts' );
 

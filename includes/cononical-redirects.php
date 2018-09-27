f2e7fc27b2c35851d4d187f99eed7d810fdbd8c1
Ivan Lopez <ivan@10up.com>
2018-09-27 11:14:37 -0400

    update comment

--- includes/cononical-redirects.php
+++ includes/cononical-redirects.php
@@ -7,4 +7,8 @@
 
 namespace tenup;
 
+/**
+ * Remove the default shorthand redirects WordPress core implements like
+ * /login, /admin, and / dashboard.
+ */
 remove_action( 'template_redirect', 'wp_redirect_admin_locations', 1000 );

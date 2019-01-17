d75a33b2a26a8a848cf3b468adf684830c637b7f
Ben Greeley <ben.greeley@10up.com>
2019-01-17 09:52:44 -0500

    Add check for 10up domain names to the author check to bypass the author verification fucntionality

--- includes/authors.php
+++ includes/authors.php
@@ -13,28 +13,45 @@ namespace tenup;
  */
 function maybe_disable_author_archive() {
 
-	if ( is_author() ){
+	if ( ! is_author() ) {
+		return;
+	}
 
-		$is_author_disabled = false;
-		$author 			= get_queried_object();
+	$is_author_disabled = false;
+	$author 			 = get_queried_object();
+	$current_domain		 = parse_url( get_site_url(), PHP_URL_HOST );
 
-		// If the query object doesn't have a user e-mail address or the filter is allowing 10up authors, bail
-		if ( empty( $author->data->user_email ) ||
-			 true === apply_filters( 'tenup_experience_allow_tenupauthor_pages', false ) ) {
-			return;
-		}
+	// Domain names that are whitelisted allowed to index 10up user blog indexes.
+	$whitelisted_domains = [
+		'10up.com',
+		'elasticpress.io',
+		'10uplabs.com',
+	];
 
-		// E-mail addresses containing 10up.com (get10up.com inclusive) will be filtered out on the front-end
-		if ( false !== stripos( $author->data->user_email, '10up.com' ) ) {
-			$is_author_disabled = true;
-		}
+	// Perform partial match on domains to catch subdomains or variation of domain name
+	$filtered_domains = array_filter( $whitelisted_domains, function( $domain ) use ( $current_domain ) {
+		return false !== stripos( $current_domain, $domain );
+	} );
 
-		// Filter tenup_experience_allow_tenupauthor_pages defaults to false. If set to true, allows 10up
-		// author accounts to load
-		if ( true  === $is_author_disabled ) {
-				\wp_safe_redirect( '/', '301' );
-				exit();
-		}
+	// If the query object doesn't have a user e-mail address or the filter is allowing 10up authors, bail
+	if ( ! empty( $filtered_domains ) ||
+		 empty( $author->data->user_email ) ||
+		 true === apply_filters( 'tenup_experience_allow_tenupauthor_pages', false ) ) {
+
+		return;
+
+	}
+
+	// E-mail addresses containing 10up.com (get10up.com inclusive) will be filtered out on the front-end
+	if ( false !== stripos( $author->data->user_email, '10up.com' ) ) {
+		$is_author_disabled = true;
+	}
+
+	// Filter tenup_experience_allow_tenupauthor_pages defaults to false. If set to true, allows 10up
+	// author accounts to load
+	if ( true  === $is_author_disabled ) {
+		\wp_safe_redirect( '/', '301' );
+		exit();
 	}
 
 	return;

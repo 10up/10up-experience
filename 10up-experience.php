356e6df00937bb2d34df032ad9c8aa15a0a3d380
John Watkins <johnwatkins0@gmail.com>
2022-10-05 09:45:25 -0500

    Eliminate redundant 'setup' calls

--- 10up-experience.php
+++ 10up-experience.php
@@ -75,20 +75,20 @@ $network_activated = Utils\is_network_activated( plugin_basename( __FILE__ ) );
 define( 'TENUP_EXPERIENCE_IS_NETWORK', (bool) $network_activated );
 
 if ( ! defined( 'TENUP_DISABLE_BRANDING' ) || ! TENUP_DISABLE_BRANDING ) {
-	AdminCustomizations\Customizations::instance()->setup();
+	AdminCustomizations\Customizations::instance();
 }
 
-API\API::instance()->setup();
-Authentication\Usernames::instance()->setup();
-Authors\Authors::instance()->setup();
-Gutenberg\Gutenberg::instance()->setup();
-Headers\Headers::instance()->setup();
-Plugins\Plugins::instance()->setup();
-PostPasswords\PostPasswords::instance()->setup();
-SupportMonitor\Monitor::instance()->setup();
-SupportMonitor\Debug::instance()->setup();
-SupportMonitor\ActivityLog::instance()->setup();
-Notifications\Welcome::instance()->setup();
+API\API::instance();
+Authentication\Usernames::instance();
+Authors\Authors::instance();
+Gutenberg\Gutenberg::instance();
+Headers\Headers::instance();
+Plugins\Plugins::instance();
+PostPasswords\PostPasswords::instance();
+SupportMonitor\Monitor::instance();
+SupportMonitor\Debug::instance();
+SupportMonitor\ActivityLog::instance();
+Notifications\Welcome::instance();
 
 /**
  * We load this later to make sure there are no conflicts with other plugins.
@@ -96,8 +96,8 @@ Notifications\Welcome::instance()->setup();
 add_action(
 	'plugins_loaded',
 	function() {
-		Authentication\Passwords::instance()->setup();
-		SSO\SSO::instance()->setup();
+		Authentication\Passwords::instance();
+		SSO\SSO::instance();
 	}
 );
 

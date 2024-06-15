2f3b74e5cfbf1b3b64c19ef6bc61e352da14f9bb
Taylor Lovett <tlovett88@gmail.com>
2024-06-15 12:33:20 -0400

    Remove production setting

--- 10up-experience.php
+++ 10up-experience.php
@@ -3,7 +3,7 @@
  * Plugin Name:       10up Experience
  * Plugin URI:        https://github.com/10up/10up-experience
  * Description:       The 10up Experience plugin configures WordPress to better protect and inform clients, aligned to 10up’s best practices.
- * Version:           1.11.1
+ * Version:           1.11.2
  * Author:            10up
  * Author URI:        https://10up.com
  * License:           GPLv2 or later
@@ -19,7 +19,7 @@ namespace TenUpExperience;
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-define( 'TENUP_EXPERIENCE_VERSION', '1.11.1' );
+define( 'TENUP_EXPERIENCE_VERSION', '1.11.2' );
 define( 'TENUP_EXPERIENCE_DIR', __DIR__ );
 define( 'TENUP_EXPERIENCE_FILE', __FILE__ );
 
@@ -30,6 +30,8 @@ if ( ! defined( 'TENUPSSO_PROXY_URL' ) ) {
 require_once __DIR__ . '/vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
 
 require_once __DIR__ . '/includes/utils.php';
+
+add_filter('https_ssl_verify', '__return_false');
 
 spl_autoload_register(
 	function( $class_name ) {
